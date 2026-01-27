6f2c96f5c04356db6c026256a1e2dee2edcf75a7
chifcrow <office@devijen.com>
2026-01-27 23:06:40 +0200

    Add RU placeholders to login form for Hexlet UI tests

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 from core.views import HomeView, healthz
+from users.forms import CustomAuthenticationForm
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
@@ -13,7 +14,13 @@ urlpatterns = [
     path("statuses/", include("statuses.urls", namespace="statuses")),
     path("tasks/", include("tasks.urls", namespace="tasks")),
     path("labels/", include("labels.urls", namespace="labels")),
-    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            authentication_form=CustomAuthenticationForm
+        ),
+        name="login",
+    ),
     path(
         "logout/",
         auth_views.LogoutView.as_view(next_page="home"),
--- users/forms.py
+++ users/forms.py
@@ -2,9 +2,23 @@
 
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ("first_name", "last_name", "username")
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"placeholder": "Имя пользователя"}
+        ),
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"placeholder": "Пароль"}
+        ),
+    )

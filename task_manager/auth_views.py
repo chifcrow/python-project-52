2fd69667357686d8b2589ec613c32cf4dd595bda
chifcrow <office@devijen.com>
2026-01-31 13:00:56 +0200

    Add flash messages for login/logout (Hexlet UI tests)

---
+++ task_manager/auth_views.py
@@ -0,0 +1,19 @@
+# task_manager/auth_views.py
+
+from django.contrib import messages
+from django.contrib.auth import views as auth_views
+from django.http import HttpRequest, HttpResponse
+
+
+class CustomLoginView(auth_views.LoginView):
+    def form_valid(self, form) -> HttpResponse:
+        response = super().form_valid(form)
+        messages.success(self.request, "Вы вошли в систему")
+        return response
+
+
+class CustomLogoutView(auth_views.LogoutView):
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        response = super().post(request, *args, **kwargs)
+        messages.info(request, "Вы вышли из системы")
+        return response
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,11 +1,10 @@
 # task_manager/urls.py
 
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 from core.views import HomeView, healthz
-from users.forms import CustomAuthenticationForm
+from task_manager.auth_views import CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
@@ -14,17 +13,7 @@ urlpatterns = [
     path("statuses/", include("statuses.urls", namespace="statuses")),
     path("tasks/", include("tasks.urls", namespace="tasks")),
     path("labels/", include("labels.urls", namespace="labels")),
-    path(
-        "login/",
-        auth_views.LoginView.as_view(
-            authentication_form=CustomAuthenticationForm
-        ),
-        name="login",
-    ),
-    path(
-        "logout/",
-        auth_views.LogoutView.as_view(next_page="home"),
-        name="logout",
-    ),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", CustomLogoutView.as_view(next_page="home"), name="logout"),
     path("admin/", admin.site.urls),
 ]
